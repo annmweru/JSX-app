903c51ba92ccbf2d7a3171d31ff1d41cc5393ba6
annmweru <annmweru9@gmail.com>
2020-06-01 22:58:24 +0300

    reactjs program

---
+++ hello-world/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+         WELCOME TO MY FIRST REACT APP
+         </h1>
+<p>skill:
+  reading,swimmimg,eating,coding:
+</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Love learning coding
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
